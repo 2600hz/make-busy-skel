d89ebe14fda1687c4bdeee18c8455739dd74f097
Roman Galeev <jamhed@2600hz.com>
2016-11-16 19:10:02 +0100

    test runner examples

--- run-test.sh
+++ run-test.sh
@@ -4,4 +4,4 @@ then
 	echo Please specify the test file
 	exit
 fi
-docker exec -ti makebusy.kazoo ./run-test.sh  tests/KazooTests/Applications/$1
+docker exec -ti makebusy.kazoo ./run-test.sh tests/KazooTests/Applications/$1
